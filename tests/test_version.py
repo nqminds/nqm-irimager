155aaeee177c6415c491abecffde0904c7d606c2
Alois Klink <alois@nquiringminds.com>
2023-08-04 10:48:01 +0100

    feat: add PEP 396 `__version__` attribute
    Add a [PEP 396][] `__version__` attribute to `nqm.irimager`.
    
    [PEP 396]: https://peps.python.org/pep-0396/

---
+++ tests/test_version.py
@@ -0,0 +1,19 @@
+"""Tests for nqm.irimager.__version__"""
+import pathlib
+import sys
+
+if sys.version_info < (3, 11):
+    import tomli as tomllib
+else:
+    import tomllib
+
+import nqm.irimager
+
+def test_version():
+    """Test whether the PEP 396 __version__ attribute is correctly set"""
+    with (pathlib.Path(__file__).parent.parent / "pyproject.toml").open("rb") as pyproject_file:
+        data = tomllib.load(pyproject_file)
+    expected_version = data["project"]["version"]
+    actual_version = nqm.irimager.__version__
+
+    assert actual_version == expected_version
