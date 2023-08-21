07ef61e4819050039d10bc13a5c0ac00a7c71fb9
Alois Klink <alois@nquiringminds.com>
2023-08-21 20:04:48 +0100

    style: run black on `tests/test_version`
    Auto-generated with `pdm run pre-commit run --all`.

--- tests/test_version.py
+++ tests/test_version.py
@@ -9,9 +9,12 @@ else:
 
 import nqm.irimager
 
+
 def test_version():
     """Test whether the PEP 396 __version__ attribute is correctly set"""
-    with (pathlib.Path(__file__).parent.parent / "pyproject.toml").open("rb") as pyproject_file:
+    with (pathlib.Path(__file__).parent.parent / "pyproject.toml").open(
+        "rb"
+    ) as pyproject_file:
         data = tomllib.load(pyproject_file)
     expected_version = data["project"]["version"]
     actual_version = nqm.irimager.__version__
