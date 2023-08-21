363ce1d69fe0e2d3dd978f1d5a82010d8e69d39f
Alois Klink <alois@nquiringminds.com>
2023-08-21 19:55:09 +0100

    style: format C++ code through clang-format
    Generated by running `pdm run pre-commit run --all src/`.

--- src/nqm/irimager/irimager.cpp
+++ src/nqm/irimager/irimager.cpp
@@ -1,6 +1,6 @@
-#include <pybind11/pybind11.h>
-#include <pybind11/numpy.h>
 #include <pybind11/chrono.h>
+#include <pybind11/numpy.h>
+#include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 #include <pybind11/stl/filesystem.h>
 #include <pybind11/stl_bind.h>
@@ -18,30 +18,41 @@
 #include DOCSTRINGS_H
 
 PYBIND11_MODULE(irimager, m) {
-    m.doc() = R"(Optris PI and XI imager IR camera controller
+  m.doc() = R"(Optris PI and XI imager IR camera controller
 
 We use the IRImagerDirect SDK
 (see http://documentation.evocortex.com/libirimager2/html/index.html)
 to control these cameras.)";
 
-    m.attr("__version__") = SKBUILD_PROJECT_VERSION;
+  m.attr("__version__") = SKBUILD_PROJECT_VERSION;
 
-    pybind11::class_<IRImager>(m, "IRImager", DOC(IRImager))
-        .def(pybind11::init<const std::filesystem::path &>(), DOC(IRImager, IRImager))
-        .def("get_frame", &IRImager::get_frame, DOC(IRImager, get_frame))
-        .def("get_temp_range_decimal", &IRImager::get_temp_range_decimal, DOC(IRImager, get_temp_range_decimal))
-        .def("get_library_version", &IRImager::get_library_version, DOC(IRImager, get_library_version))
-        .def("start_streaming", &IRImager::start_streaming, DOC(IRImager, start_streaming))
-        .def("stop_streaming", &IRImager::stop_streaming, DOC(IRImager, stop_streaming))
-        .def("__enter__", &IRImager::_enter_, pybind11::return_value_policy::reference_internal)
-        .def("__exit__", &IRImager::_exit_);
+  pybind11::class_<IRImager>(m, "IRImager", DOC(IRImager))
+      .def(pybind11::init<const std::filesystem::path &>(),
+           DOC(IRImager, IRImager))
+      .def("get_frame", &IRImager::get_frame, DOC(IRImager, get_frame))
+      .def("get_temp_range_decimal", &IRImager::get_temp_range_decimal,
+           DOC(IRImager, get_temp_range_decimal))
+      .def("get_library_version", &IRImager::get_library_version,
+           DOC(IRImager, get_library_version))
+      .def("start_streaming", &IRImager::start_streaming,
+           DOC(IRImager, start_streaming))
+      .def("stop_streaming", &IRImager::stop_streaming,
+           DOC(IRImager, stop_streaming))
+      .def("__enter__", &IRImager::_enter_,
+           pybind11::return_value_policy::reference_internal)
+      .def("__exit__", &IRImager::_exit_);
 
-    pybind11::class_<IRImagerMock, IRImager>(m, "IRImagerMock", DOC(IRImagerMock))
-        .def(pybind11::init<const std::filesystem::path &>(), DOC(IRImager, IRImager))
-        .def("get_frame", &IRImagerMock::get_frame, DOC(IRImager, get_frame))
-        .def("get_temp_range_decimal", &IRImagerMock::get_temp_range_decimal, DOC(IRImager, get_temp_range_decimal))
-        .def("start_streaming", &IRImagerMock::start_streaming, DOC(IRImager, start_streaming))
-        .def("stop_streaming", &IRImagerMock::stop_streaming, DOC(IRImager, stop_streaming))
-        .def("__enter__", &IRImagerMock::_enter_, pybind11::return_value_policy::reference_internal)
-        .def("__exit__", &IRImagerMock::_exit_);
+  pybind11::class_<IRImagerMock, IRImager>(m, "IRImagerMock", DOC(IRImagerMock))
+      .def(pybind11::init<const std::filesystem::path &>(),
+           DOC(IRImager, IRImager))
+      .def("get_frame", &IRImagerMock::get_frame, DOC(IRImager, get_frame))
+      .def("get_temp_range_decimal", &IRImagerMock::get_temp_range_decimal,
+           DOC(IRImager, get_temp_range_decimal))
+      .def("start_streaming", &IRImagerMock::start_streaming,
+           DOC(IRImager, start_streaming))
+      .def("stop_streaming", &IRImagerMock::stop_streaming,
+           DOC(IRImager, stop_streaming))
+      .def("__enter__", &IRImagerMock::_enter_,
+           pybind11::return_value_policy::reference_internal)
+      .def("__exit__", &IRImagerMock::_exit_);
 }
--- src/nqm/irimager/irimager_class.cpp
+++ src/nqm/irimager/irimager_class.cpp
@@ -1,113 +1,108 @@
 #include "./irimager_class.hpp"
+
 #include "spdlog/spdlog.h"
 
 struct IRImager::impl {
-public:
-    impl() = default;
-    impl(const std::filesystem::path &xml_path) {
-        // do a basic check that the given file is readable, and is an XML file
-        auto xml_stream = std::ifstream(xml_path, std::fstream::in);
-        auto xml_header = std::string(5, '\0');
+ public:
+  impl() = default;
+  impl(const std::filesystem::path &xml_path) {
+    // do a basic check that the given file is readable, and is an XML file
+    auto xml_stream = std::ifstream(xml_path, std::fstream::in);
+    auto xml_header = std::string(5, '\0');
 
-        xml_stream.read(xml_header.data(), static_cast<std::streamsize>(xml_header.size()));
-        if (xml_header != std::string("<?xml")) {
-            throw std::runtime_error("Invalid XML file: The given XML file does not start with '<?xml'");
-        }
+    xml_stream.read(xml_header.data(),
+                    static_cast<std::streamsize>(xml_header.size()));
+    if (xml_header != std::string("<?xml")) {
+      throw std::runtime_error(
+          "Invalid XML file: The given XML file does not start with '<?xml'");
+    }
+  }
+
+  /** @copydoc IRImager::start_streaming() */
+  virtual void start_streaming() { streaming = true; }
+
+  /** @copydoc IRImager::stop_streaming() */
+  virtual void stop_streaming() { streaming = false; }
+
+  /** @copydoc IRImager::get_frame() */
+  virtual std::tuple<pybind11::array_t<uint16_t>,
+                     std::chrono::system_clock::time_point>
+  get_frame() {
+    if (!streaming) {
+      throw std::runtime_error("IRIMAGER_STREAMOFF: Not streaming");
     }
 
-    /** @copydoc IRImager::start_streaming() */
-    virtual void start_streaming() {
-        streaming = true;
+    auto frame_size = std::array<ssize_t, 2>{382, 288};
+    auto my_array = pybind11::array_t<uint16_t>(frame_size);
+
+    auto r = my_array.mutable_unchecked<frame_size.size()>();
+
+    for (ssize_t i = 0; i < frame_size[0]; i++) {
+      for (ssize_t j = 0; j < frame_size[1]; j++) {
+        r(i, j) = static_cast<uint16_t>((1800 + 100) *
+                                        std::pow(10, get_temp_range_decimal()));
+      }
     }
 
-    /** @copydoc IRImager::stop_streaming() */
-    virtual void stop_streaming() {
-        streaming = false;
-    }
+    return std::make_tuple(my_array, std::chrono::system_clock::now());
+  }
 
-    /** @copydoc IRImager::get_frame() */
-    virtual std::tuple<
-        pybind11::array_t<uint16_t>,
-        std::chrono::system_clock::time_point
-    > get_frame() {
-        if (!streaming) {
-            throw std::runtime_error("IRIMAGER_STREAMOFF: Not streaming");
-        }
+  /** @copydoc IRImager::get_temp_range_decimal() */
+  virtual short get_temp_range_decimal() { return 1; }
 
-        auto frame_size = std::array<ssize_t, 2>{382, 288};
-        auto my_array = pybind11::array_t<uint16_t>(frame_size);
+  /** @copydoc IRImager::get_library_version() */
+  virtual std::string_view get_library_version() = 0;
 
-        auto r = my_array.mutable_unchecked<frame_size.size()>();
+  virtual ~impl() = default;
 
-        for (ssize_t i = 0; i < frame_size[0]; i++) {
-            for (ssize_t j = 0; j < frame_size[1]; j++) {
-                r(i, j) = static_cast<uint16_t>(
-                    (1800 + 100) * std::pow(10, get_temp_range_decimal())
-                );
-            }
-        }
-
-        return std::make_tuple(my_array, std::chrono::system_clock::now());
-    }
-
-    /** @copydoc IRImager::get_temp_range_decimal() */
-    virtual short get_temp_range_decimal() {
-        return 1;
-    }
-
-    /** @copydoc IRImager::get_library_version() */
-    virtual std::string_view get_library_version() = 0;
-
-    virtual ~impl() = default;
-
-protected:
-    bool streaming = false;
+ protected:
+  bool streaming = false;
 };
 
 /**
  * Mocked implentation, doesn't use irimager or a real camera.
  */
-struct IRImagerMockImpl final: public IRImager::impl {
-public:
-    IRImagerMockImpl() {
-        spdlog::warn("Creating a MOCKED IRImager object!");
-    }
-    IRImagerMockImpl(const IRImager::impl &other): IRImager::impl(other) {
-        spdlog::warn("Creating a MOCKED IRImager object!");
-    }
-    IRImagerMockImpl(const std::filesystem::path &xml_path): IRImager::impl(xml_path) {
-        spdlog::warn("Creating a MOCKED IRImager object!");
-    }
+struct IRImagerMockImpl final : public IRImager::impl {
+ public:
+  IRImagerMockImpl() { spdlog::warn("Creating a MOCKED IRImager object!"); }
+  IRImagerMockImpl(const IRImager::impl &other) : IRImager::impl(other) {
+    spdlog::warn("Creating a MOCKED IRImager object!");
+  }
+  IRImagerMockImpl(const std::filesystem::path &xml_path)
+      : IRImager::impl(xml_path) {
+    spdlog::warn("Creating a MOCKED IRImager object!");
+  }
 
-    std::string_view get_library_version() override {
-        return "MOCKED";
-    }
+  std::string_view get_library_version() override { return "MOCKED"; }
 
-    virtual ~IRImagerMockImpl() = default;
+  virtual ~IRImagerMockImpl() = default;
 };
 
 #ifdef IR_IMAGER_MOCK
 #define IRImagerDefaultImplementation IRImagerMockImpl
-#else // not mocked
+#else  // not mocked
 
 #include "libirimager/IRDevice.h"
 #include "libirimager/IRImager.h"
 
 /**
- * Real implentation, requires both a working irimager library, camera, and calibration data.
+ * Real implentation, requires both a working irimager library, camera, and
+ * calibration data.
  */
-struct IRImagerRealImpl final: public IRImager::impl {
-public:
-    IRImagerRealImpl() = default;
-    IRImagerRealImpl(const IRImager::impl &other): IRImager::impl(other) {}
-    IRImagerRealImpl(const std::filesystem::path &xml_path): IRImager::impl(xml_path) {}
-    virtual ~IRImagerRealImpl() = default;
+struct IRImagerRealImpl final : public IRImager::impl {
+ public:
+  IRImagerRealImpl() = default;
+  IRImagerRealImpl(const IRImager::impl &other) : IRImager::impl(other) {}
+  IRImagerRealImpl(const std::filesystem::path &xml_path)
+      : IRImager::impl(xml_path) {}
+  virtual ~IRImagerRealImpl() = default;
 
-    std::string_view get_library_version() override {
-        return evo::IRImager::getVersion();
-    }
-private:
-    std::shared_ptr<evo::IRDevice> ir_device;
+  std::string_view get_library_version() override {
+    return evo::IRImager::getVersion();
+  }
+
+ private:
+  std::shared_ptr<evo::IRDevice> ir_device;
 };
 
 #define IRImagerDefaultImplementation IRImagerRealImpl
@@ -115,50 +110,43 @@ private:
 
 IRImager::IRImager() = default;
 
-IRImager::IRImager(const IRImager& other): pImpl{std::make_unique<IRImagerDefaultImplementation>(*other.pImpl)} {}
+IRImager::IRImager(const IRImager &other)
+    : pImpl{std::make_unique<IRImagerDefaultImplementation>(*other.pImpl)} {}
 
-IRImager::IRImager(IRImager&& other) = default;
+IRImager::IRImager(IRImager &&other) = default;
 
-IRImager::IRImager(const std::filesystem::path &xml_path): pImpl{std::make_unique<IRImagerDefaultImplementation>(xml_path)} {}
+IRImager::IRImager(const std::filesystem::path &xml_path)
+    : pImpl{std::make_unique<IRImagerDefaultImplementation>(xml_path)} {}
 
 IRImager::~IRImager() = default;
 
-void IRImager::start_streaming() {
-    pImpl->start_streaming();
-}
+void IRImager::start_streaming() { pImpl->start_streaming(); }
 
-void IRImager::stop_streaming() {
-    pImpl->stop_streaming();
-}
+void IRImager::stop_streaming() { pImpl->stop_streaming(); }
 
-IRImager* IRImager::_enter_() {
-    start_streaming();
-    return this;
+IRImager *IRImager::_enter_() {
+  start_streaming();
+  return this;
 }
 
 void IRImager::_exit_(
     [[maybe_unused]] const std::optional<pybind11::type> &exc_type,
     [[maybe_unused]] const std::optional<pybind11::object> &exc_value,
     [[maybe_unused]] const std::optional<pybind11::object> &traceback) {
-
-    stop_streaming();
+  stop_streaming();
 }
 
-std::tuple<
-    pybind11::array_t<uint16_t>,
-    std::chrono::system_clock::time_point
-> IRImager::get_frame() {
-    return pImpl->get_frame();
+std::tuple<pybind11::array_t<uint16_t>, std::chrono::system_clock::time_point>
+IRImager::get_frame() {
+  return pImpl->get_frame();
 }
 
-short IRImager::get_temp_range_decimal() {
-    return 1;
-}
+short IRImager::get_temp_range_decimal() { return 1; }
 
 std::string_view IRImager::get_library_version() {
-    return pImpl->get_library_version();
+  return pImpl->get_library_version();
 }
 
 IRImagerMock::IRImagerMock(const std::filesystem::path &xml_path) {
-    pImpl = std::make_unique<IRImagerMockImpl>(xml_path);
+  pImpl = std::make_unique<IRImagerMockImpl>(xml_path);
 }
